43c7374246054f1593f5b8ca8fc761a22e9f0719
Ashlurmp <ashleylaurel@outlook.com>
2019-07-10 21:16:14 +0100

    fixing the like button, the blog content, the social media tiles etc.

---
+++ js/Scripts.js
@@ -0,0 +1,17 @@
+var like1 = document.getElementById("like-1")
+like1.onclick = function () {
+    this.style.backgroundColor = "#007bff";
+    this.style.color = "white";
+};
+
+var like2 = document.getElementById("like-2")
+like2.onclick = function () {
+    this.style.backgroundColor = "#007bff";
+    this.style.color = "white";
+};
+
+var like3 = document.getElementById("like-3")
+like3.onclick = function () {
+    this.style.backgroundColor = "#007bff";
+    this.style.color = "white";
+};
